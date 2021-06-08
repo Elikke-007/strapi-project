61829d95f56d105689894711ff4fa438299c7f61
sunwenzhong <sun591854735@qq.com>
2021-06-08 19:57:56 +0800

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'd3e9be27b314f8246a7fa34d1825e278'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '20d1f816-5108-4b52-ace8-8271f3563e8d'
+};
